8f8eb58905393373dc633b182daeeb2eb3d76a9f
xlwang <xlwang@piperi.cs.umass.edu>
2014-08-20 14:19:13 -0400

    Revert "Cplex bug fixed"
    This reverts commit 20f4f4a5b7b6ae2d924d8bc79dfafd76df71ca53.

--- src/queryprovenance/query/CplexHandler.java
+++ src/queryprovenance/query/CplexHandler.java
@@ -35,11 +35,8 @@ public class CplexHandler {
 		// solve cplex
 		if(cplex.solve()){
 
-            for(int i = 0; i < fixed_values.length; ++i){
-				int digits = (int) Math.pow(10, (double) (String.valueOf(epsilon).length() - String.valueOf(epsilon).lastIndexOf(".") + 1));
-				fixed_values[i] = (double) Math.round(cplex.getValue(var[i])*digits)/digits;
-			}
-            
+			for(int i = 0; i < fixed_values.length; ++i)
+				fixed_values[i] = (double) cplex.getValue(var[i]);
 			return this.toConditionRules(where, fixed_values);
 		}
 		else
