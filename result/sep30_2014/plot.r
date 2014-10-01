72733e075a82d9d29b9e6766cef381c09d848566
eugene wu <sirrice@gmail.com>
2014-10-01 00:25:23 -0400

    fixed big bug in plotting code

--- result/sep30_2014/plot.r
+++ result/sep30_2014/plot.r
@@ -24,7 +24,8 @@ data = ddply(basedata,
 #data2 = data[data$cardinality %in% c(1, 5) & data$logsize %in% c(5, 20),]
 #basedata2 = basedata[basedata$cardinality %in% c(1, 5) & basedata$logsize %in% c(5, 20),]
 
-plotit = function(colname, basedat, dat) {
+plotit = function(aes, basedat, dat) {
+  colname = paste(aes$y)
   yname = colname
   sy = scale_y_continuous
   if (max(dat[,colname]) - min(dat[,colname]) > 1000) {
@@ -34,35 +35,39 @@ plotit = function(colname, basedat, dat) {
   jitter = position_jitter(width = .08, height= .05)
   data = dat
   basedata = basedat
-  a = aes(x=dbsize, color=solver, y=data[,colname])
+  a = aes(x=dbsize, color=solver)
+  a$y = aes$y
   p = ggplot(data, a, environment=environment())
-  p = p + geom_point(data=basedata, aes(x=dbsize, y=basedata[,colname]), alpha=0.5, position=jitter)
+  p = p + geom_point(data=basedata, a, alpha=0.5, position=jitter)
   p = p + geom_point(color='black') + geom_line() + facet_grid(logsize~cardinality, labeller=paste)
   p = p + sy(name=colname)
   ggsave(paste('pdfs/',colname, '_vs_dbsize.pdf', sep=''), p, scale=1.5)
 
 
-  #data2 = data[data$cardinality %in% c(1, 5),]# & data$logsize %in% c(5, 20),]
-  p = ggplot(data, aes(x=logsize, y=data[,colname], color=solver), environment=environment()) 
-  p = p + geom_point(data=basedata, aes(x=logsize, y=basedata[,colname]), alpha=0.5, position=jitter)
+  a = aes(x=logsize, color=solver)
+  a$y = aes$y
+  p = ggplot(data, a, environment=environment()) 
+  p = p + geom_point(data=basedata, a, alpha=0.5, position=jitter)
   p = p + geom_point(color='black') + geom_line() + facet_grid(dbsize~cardinality, labeller=paste)
   p = p + sy(name=colname)
   ggsave(paste('pdfs/',colname, '_vs_logsize.pdf', sep=''), p, scale=1.5)
 
-  p = ggplot(data, aes(x=cardinality, y=data[,colname], color=solver), environment=environment()) 
-  p = p + geom_point(data=basedata, aes(x=cardinality, y=basedata[,colname]), alpha=0.5, position=jitter)
+  a = aes(x=cardinality, color=solver)
+  a$y = aes$y
+  p = ggplot(data, a, environment=environment()) 
+  p = p + geom_point(data=basedata, a, alpha=0.5, position=jitter)
   p = p + geom_point(color='black') + geom_line() + facet_grid(dbsize~logsize, labeller=paste)
   p = p + sy(name=colname)
   ggsave(paste('pdfs/',colname, '_vs_card.pdf', sep=''), p, scale=1.5)
 }
 
 
-plotit('totaltime', basedata, data)
-plotit('badcomplaints', basedata, data)
-plotit('remaincomplaints', basedata, data)
-plotit('fixedrate', basedata, data)
-plotit('noiserate', basedata, data)
-plotit('preptime', basedata, data)
-plotit('solvetime', basedata, data)
-plotit('finishtime', basedata, data)
+plotit(aes(y=totaltime), basedata, data)
+plotit(aes(y=badcomplaints), basedata, data)
+plotit(aes(y=remaincomplaints), basedata, data)
+plotit(aes(y=fixedrate), basedata, data)
+plotit(aes(y=noiserate), basedata, data)
+plotit(aes(y=preptime), basedata, data)
+plotit(aes(y=solvetime), basedata, data)
+plotit(aes(y=finishtime), basedata, data)
 
